a090980950c394506e39cbebbad8dccdaadc088b
yakisuzu <yakisuzu@gmail.com>
2015-12-03 23:19:45 +0900

    to static func of file

--- src/modules/File.js
+++ src/modules/File.js
@@ -25,14 +25,14 @@ class File{
    * @param {String}
    * @return {boolean}
    */
-  exists(st_path){
+  static exists(st_path){
     return ws_fso.FileExists(st_path);
   }
 
   /**
    * @return {void}
    */
-  createTextFile(st_file, ar_text){
+  static createTextFile(st_file, ar_text){
     let ws_file = ws_fso.CreateTextFile(st_file, true);
     for(let st_text of ar_text){
       ws_file.WriteLine(st_text);
